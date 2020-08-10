347b69feee608e0a03e11b3e55ce688e8b0eaaa6
venus <venus@venusworld.cn>
2020-08-11 03:03:19 +0800

    feat(chapter 5): ray 与球体相交时，算出 ray 的相交点，以便计算球体的法线

--- 05.surface-normals-and-multiple-objects.cpp
+++ 05.surface-normals-and-multiple-objects.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 #include "ray.h"
 
-bool hit_sphere(const vec3& center, float radius, const ray& r) {
+float hit_sphere(const vec3& center, float radius, const ray& r) {
     vec3 oc = r.origin() - center;
     float a = dot(r.direction(), r.direction());
     float b = 2.0 * dot(oc, r.direction());
     float c = dot(oc, oc) - radius*radius;
     float discriminant = b*b - 4*a*c;
-    return (discriminant > 0);
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - sqrt(discriminant)) / (2.0*a);
+    }
 }
 
 vec3 color(const ray& r) {
-    if (hit_sphere(vec3(0,0,-1), 0.5, r)) {  // 检测 ray 是否与位于 [0,0,-1]，半径为 0.5 的球相交
-                                             // 如果你把球心 z 设为 +1，那么你会得到相同的图像，因为你现在能够看到你背后的对象，我们之后会修复这个问题。
-        return vec3(1.0, 0.0, 0.0);
+    float t = hit_sphere(vec3(0,0,-1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0,0,-1));
+        return 0.5*vec3(N.x()+1, N.y()+1, N.z()+1);
     }
     vec3 unit_direction = unit_vector(r.direction());
-    float t = 0.5*(unit_direction.y() + 1.0);
+    t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
 }
 
