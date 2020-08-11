b1dcc946600a95836738b17f022a310403f24bae
venus <venus@venusworld.cn>
2020-08-12 04:25:48 +0800

    feat(chapter 7): diffuse materials

--- 07.diffuse-materials.cpp
+++ 07.diffuse-materials.cpp
@@ -5,10 +5,22 @@
 #include "camera.h"
 #include "float.h"
 
+vec3 random_in_unit_sphere() {
+    vec3 p;
+    do {
+        // [0, +2] => [-1, +1]
+        p = 2.0*vec3(drand48(),drand48(),drand48()) - vec3(1.0,1.0,1.0);
+    } while (p.squared_length() >= 1.0);
+    return p;
+}
+
 vec3 color(const ray& r, hitable *world) {
     hit_record rec;
-    if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-        return 0.5*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+    if (world->hit(r, 0.001, MAXFLOAT, rec)) {
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+        // 从相交点再发射出一条光线，方向为从相交点指向相交点正切的单位球内的点
+        // 光线的颜色衰减为原来的 0.5 倍
+        return 0.5*color( ray(rec.p, target-rec.p), world);
     } else {
         vec3 unit_direction = unit_vector(r.direction());
         float t = 0.5*(unit_direction.y() + 1.0);
@@ -40,6 +52,8 @@ int main() {
                 col += color(r, world);
             }
             col /= float(ns);
+            // gamma corrected
+            col = vec3( sqrt(col[0]), sqrt(col[1]), sqrt(col[2]) );
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
