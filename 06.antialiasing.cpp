8491d84b9d8ae8cbefc8803e4214fdb189e29da0
venus <venus@venusworld.cn>
2020-08-11 21:12:50 +0800

    feat(chapter 6): 添加抗锯齿，每个像素发送多条 ray，然后取颜色的平局值

--- 06.antialiasing.cpp
+++ 06.antialiasing.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <random>
 #include "sphere.h"
 #include "hitable_list.h"
+#include "camera.h"
 #include "float.h"
 
 vec3 color(const ray& r, hitable *world) {
@@ -17,24 +19,27 @@ vec3 color(const ray& r, hitable *world) {
 int main() {
     int nx = 200;
     int ny = 100;
+    int ns = 100;  // 每个像素的采样次数
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-    vec3 lower_left_corner(-2.0, -1.0, -1.0);  // 屏幕左下角的位置在 3D 场景内的坐标
-    vec3 horizontal(4.0, 0.0, 0.0);            // 屏幕的宽度
-    vec3 vertical(0.0, 2.0, 0.0);              // 屏幕的高度
-    vec3 origin(0.0, 0.0, 0.0);                // eye 的原点
 
     hitable *list[2];
     list[0] = new sphere(vec3(0,0,-1), 0.5);
     list[1] = new sphere(vec3(0,-100.5,-1), 100);
     hitable *world = new hitable_list(list, 2);
 
-    for (int j = ny-1; j >= 0; j--) {
-        for (int i = 0; i < nx; i++) {
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+    camera cam;
 
-            vec3 col = color(r, world);
+    for (int j = ny-1; j >= 0; j--) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < nx; i++) {
+            vec3 col(0, 0, 0);
+            for (int s = 0; s < ns; s++) {
+                float u = float(i + drand48()) / float(nx);
+                float v = float(j + drand48()) / float(ny);
+                ray r = cam.get_ray(u, v);
+                col += color(r, world);
+            }
+            col /= float(ns);
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
@@ -42,4 +47,6 @@ int main() {
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
+
+    std::cerr << "\nDone.\n";
 }
