23d3d6a34d2360297b8c5284a05b4362d1525a27
venus <venus@venusworld.cn>
2020-08-17 00:44:28 +0800

    feat(chapter 15): add checker texture

--- 15.solid-textures.cpp
+++ 15.solid-textures.cpp
@@ -4,12 +4,14 @@
 #include "hitable_list.h"
 #include "camera.h"
 #include "material.h"
+#include "texture.h"
 #include "float.h"
 
 hitable *random_scene() {
     int n = 50000;
     hitable **list = new hitable*[n+1];
-    list[0] = new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+    texture *checker = new checker_texture(new constant_texture(vec3(0.2, 0.3, 0.1)), new constant_texture(vec3(0.9, 0.9, 0.9)));
+    list[0] = new sphere(vec3(0,-1000,0), 1000, new lambertian(checker));
 
     int i = 1;
     for (int a = -10; a < 10; a++) {
@@ -19,7 +21,7 @@ hitable *random_scene() {
             if ((center-vec3(4,0.2,0)).length() > 0.9) {
                 if (choose_mat < 0.8) { // diffuse
                     list[i++] = new moving_sphere(center, center+vec3(0,0.5*drand48(),0), 0.0, 1.0, 0.2,
-                            new lambertian(vec3(drand48()*drand48(), drand48()*drand48(), drand48()*drand48() )));
+                            new lambertian(new constant_texture( vec3(drand48()*drand48(), drand48()*drand48(), drand48()*drand48()) )));
                 } else if (choose_mat < 0.95) { // metal
                     list[i++] = new sphere(center, 0.2,
                             new metal(vec3(0.5*(1 + drand48()), 0.5*(1 + drand48()), 0.5*(1 + drand48())), 0.5*drand48()));
@@ -31,7 +33,7 @@ hitable *random_scene() {
     }
 
     list[i++] = new sphere(vec3(0,1,0), 1.0, new dielectric(1.5));
-    list[i++] = new sphere(vec3(-4,1,0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+    list[i++] = new sphere(vec3(-4,1,0), 1.0, new lambertian(new constant_texture(vec3(0.4, 0.2, 0.1))));
     list[i++] = new sphere(vec3(4,1,0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
 
     return new hitable_list(list, i);
