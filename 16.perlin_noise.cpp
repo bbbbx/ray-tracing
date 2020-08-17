ca6a3ccf6dee5bcb16645c5c31b096d47b15a348
venus <venus@venusworld.cn>
2020-08-17 21:34:18 +0800

    feat(chapter 16): noise texture scalable

--- 16.perlin_noise.cpp
+++ 16.perlin_noise.cpp
@@ -8,7 +8,7 @@
 #include "float.h"
 
 hitable *two_perlin_spheres() {
-    texture *pertext = new noise_texture();
+    texture *pertext = new noise_texture(4.0);
     hitable **list = new hitable*[2];
     list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(pertext));
     list[1] = new sphere(vec3(0,     2, 0),   2, new lambertian(pertext));
