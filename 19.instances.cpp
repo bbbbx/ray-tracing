6ec16979ccbca91e98f6262e0265f8334016d5a2
venus <venus@venusworld.cn>
2020-08-19 00:02:07 +0800

    feat(chapter 19): add box primitive(not rotated)

--- 19.instances.cpp
+++ 19.instances.cpp
@@ -5,6 +5,7 @@
 #include <random>
 #include "sphere.h"
 #include "rect.h"
+#include "box.h"
 #include "hitable_list.h"
 #include "camera.h"
 #include "material.h"
@@ -12,7 +13,7 @@
 #include "float.h"
 
 hitable *cornell_box() {
-    hitable **list = new hitable*[6];
+    hitable **list = new hitable*[8];
     int i = 0;
     material *red = new lambertian(new constant_texture(vec3(0.65, 0.05, 0.05)) );
     material *white = new lambertian(new constant_texture(vec3(0.73, 0.73, 0.73)) );
@@ -24,6 +25,10 @@ hitable *cornell_box() {
     list[i++] = new flip_normals(new xz_rect(0, 555, 0, 555, 555, white));  // 顶墙
     list[i++] = new xz_rect(0, 555, 0, 555, 0, white);  // 底
     list[i++] = new flip_normals(new xy_rect(0, 555, 0, 555, 555, white));  // 背
+    
+    list[i++] = new box(vec3(130, 0, 65), vec3(295, 165, 230), white);
+    list[i++] = new box(vec3(265, 0, 295), vec3(430, 330, 460), white);
+
     return new hitable_list(list, i);
 }
 
